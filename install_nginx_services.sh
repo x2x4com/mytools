a164549181c91dbc798c996442cc6f3a129321d0
许向 <x2x4com@gmail.com>
2017-12-13 17:12:11 +0800

    fix bug

--- install_nginx_services.sh
+++ install_nginx_services.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 bk_home='/data/backup/nginx'
-target='./conf/nginx/nginx.service'
+target='nginx.service'
 systemd_dir='/lib/systemd/system'
 bk=$(date '+%Y%m%d_%H%M%S%N')
 nginx_root="/usr/local/nginx"
 
 [ ! -d "$nginx_root" ] && echo "Can not find nginx root" && exit 1
-[ ! -f "$target" ] && echo "Can not find $target" && exit 1
+[ ! -f "./conf/nginx/$target" ] && echo "Can not find ./conf/nginx/$target" && exit 1
 [ ! -d "$bk_home" ] && sudo mkdir -p $bk_home
 [ -f "$systemd_dir/$target" ] && sudo mv $systemd_dir/$target $bk_home/${target}.$bk
 sudo cp $target $systemd_dir/$target
