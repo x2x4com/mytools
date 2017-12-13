f08aec253082cb2f8a31fd2c8545dc9e476fa271
许向 <x2x4com@gmail.com>
2017-12-13 17:08:52 +0800

    fix bug

--- install_nginx_services.sh
+++ install_nginx_services.sh
@@ -6,7 +6,7 @@ systemd_dir='/lib/systemd/system'
 bk=$(date '+%Y%m%d_%H%M%S%N')
 nginx_root="/usr/local/nginx"
 
-[ ! -f "$nginx_root" ] && echo "Can not find nginx root" && exit 1
+[ ! -d "$nginx_root" ] && echo "Can not find nginx root" && exit 1
 [ ! -f "$target" ] && echo "Can not find $target" && exit 1
 [ ! -d "$bk_home" ] && sudo mkdir -p $bk_home
 [ -f "$systemd_dir/$target" ] && sudo mv $systemd_dir/$target $bk_home/${target}.$bk
@@ -17,7 +17,7 @@ sudo systemctl status nginx
 #copy example config
 conf="$nginx_root/conf"
 vhosts="$conf/vhosts"
-[ ! -f "$vhosts" ] && mkdir -p $vhosts
+[ ! -d "$vhosts" ] && mkdir -p $vhosts
 [ -f "$conf/nginx.conf" ] && mv $conf/nginx.conf $bk_home/nginx.conf.$bk
 cp ./conf/nginx/nginx.conf $conf/nginx.conf
 [ ! -f "$vhots/www.conf" ] && cp ./conf/nginx/www.conf $vhosts/www.conf
