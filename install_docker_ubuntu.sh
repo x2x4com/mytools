0e8b3f869083d34ed1eb0bd643d266eef0be6cd4
许向 <x2x4com@gmail.com>
2018-08-10 13:32:14 +0800

    update

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -2,7 +2,7 @@
 
 
 
-sudo apt-get -y remove docker docker-engine docker.io && \
+sudo apt-get -y remove docker docker-engine docker.io ; \
 sudo apt-get update && \
 sudo apt-get -y install \
     apt-transport-https \
