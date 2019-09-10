ea70d56d064b5c37ab2a5d4adf18675ed8346ea6
许向 <x2x4com@gmail.com>
2019-09-10 18:07:28 +0800

    add jq for centos

--- install_docker.sh
+++ install_docker.sh
@@ -18,7 +18,7 @@ install_centos() {
                   docker-latest-logrotate \
                   docker-logrotate \
                   docker-engine -y
-    yum install -y yum-utils device-mapper-persistent-data lvm2 && \
+    yum install -y yum-utils device-mapper-persistent-data lvm2 jq && \
     yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
     yum install docker-ce docker-ce-cli containerd.io && docker_install_success=0 
 }
